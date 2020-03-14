5c4faa8a477c571f1fca6853ebb651d1aac159d7
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-03-14 22:17:40 +0800

    add BooksList user control
    add folder for user controls

--- StonksLib.vb
+++ StonksLib.vb
@@ -1,9 +1,3 @@
 ﻿Public Class Form_StonksLib
-    Private Sub ToolStrip_BookDetails_Click(sender As Object, e As EventArgs) Handles ToolStrip_BookDetails.Click
-        If (SplitContainer_BookList.Panel2Collapsed) Then
-            SplitContainer_BookList.Panel2Collapsed = False
-        Else
-            SplitContainer_BookList.Panel2Collapsed = True
-        End If
-    End Sub
+
 End Class
