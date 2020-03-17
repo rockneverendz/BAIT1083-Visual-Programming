ee90b5fbf2185ac36efa4a9836743744863bdc2e
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-03-17 12:19:33 +0800

    test login

---
+++ Login.vb
@@ -0,0 +1,31 @@
+﻿Public Class Login
+    Dim db As New LibDBDataContext()
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username As String = txtUsername.Text
+        Dim password As String = txtPassword.Text
+
+
+        Dim chkUser = From a In db.AdminLibs
+                      Where a.Name.Contains(username) And
+                              a.Password.Contains(password)
+
+        If chkUser.Any() Then
+            lblMessage.Text = "Successfully logged in!"
+            Form_StonksLib.Show()
+
+        Else
+            lblMessage.Text = "Invalid user name or password!"
+        End If
+
+    End Sub
+
+
+End Class
