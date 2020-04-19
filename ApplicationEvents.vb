430cf50a46d27c9de557b996bac4938b1440eb9a
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-04-20 00:44:37 +0800

    Update Project generate files

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -1,3 +1,5 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
 Namespace My
     ' The following events are available for MyApplication:
     ' Startup: Raised when the application starts, before the startup form is created.
@@ -6,8 +8,9 @@ Namespace My
     ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
-        Private Sub MyApplication_Startup(sender As Object, e As ApplicationServices.StartupEventArgs) Handles Me.Startup
-            Dim path As String = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\debug\", "")
+        Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+
+            Dim path As String = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\Debug\", "")
             AppDomain.CurrentDomain.SetData("DataDirectory", path)
 
         End Sub
