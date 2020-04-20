d77437339949070189ecc34cac6d7cff51dd7caa
JY629 <52078237+JY629@users.noreply.github.com>
2020-04-20 15:09:08 +0800

    Return Book UI design

---
+++ User Controls/UserControl_ReturnBook.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl_ReturnBook
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.txtFeesPaid = New System.Windows.Forms.NumericUpDown()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnReturn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Book_Id = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Issue_Date = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Due_Date = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        CType(Me.txtFeesPaid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Book_Id, Me.Issue_Date, Me.Due_Date, Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(28, 47)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(642, 166)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ListView2
+        '
+        Me.ListView2.HideSelection = False
+        Me.ListView2.Location = New System.Drawing.Point(28, 259)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(534, 118)
+        Me.ListView2.TabIndex = 1
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'txtFeesPaid
+        '
+        Me.txtFeesPaid.Location = New System.Drawing.Point(571, 259)
+        Me.txtFeesPaid.Name = "txtFeesPaid"
+        Me.txtFeesPaid.Size = New System.Drawing.Size(102, 20)
+        Me.txtFeesPaid.TabIndex = 3
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(460, 405)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(102, 23)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnReturn
+        '
+        Me.btnReturn.Location = New System.Drawing.Point(571, 405)
+        Me.btnReturn.Name = "btnReturn"
+        Me.btnReturn.Size = New System.Drawing.Size(102, 23)
+        Me.btnReturn.TabIndex = 5
+        Me.btnReturn.Text = "Return"
+        Me.btnReturn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(568, 243)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Fee Paid"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(25, 243)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Return Notes"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(25, 31)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Item"
+        '
+        'Book_Id
+        '
+        Me.Book_Id.Text = "Title"
+        Me.Book_Id.Width = 240
+        '
+        'Issue_Date
+        '
+        Me.Issue_Date.Text = "Borrow Date"
+        Me.Issue_Date.Width = 100
+        '
+        'Due_Date
+        '
+        Me.Due_Date.Text = "Due Date"
+        Me.Due_Date.Width = 100
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Daily Late Fee"
+        Me.ColumnHeader1.Width = 100
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Owing"
+        Me.ColumnHeader2.Width = 100
+        '
+        'UserControl_ReturnBook
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnReturn)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.txtFeesPaid)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "UserControl_ReturnBook"
+        Me.Size = New System.Drawing.Size(702, 454)
+        CType(Me.txtFeesPaid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ListView2 As ListView
+    Friend WithEvents txtFeesPaid As NumericUpDown
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnReturn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Book_Id As ColumnHeader
+    Friend WithEvents Issue_Date As ColumnHeader
+    Friend WithEvents Due_Date As ColumnHeader
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+End Class
---
+++ User Controls/UserControl_ReturnBook.vb
@@ -0,0 +1,24 @@
+﻿Public Class UserControl_ReturnBook
+    Private Sub NumericUpDown_Weeks_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub DateTimePicker_DueDate_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+    Private Sub txtFeesPaid_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtFeesPaid.KeyPress
+        If (Not Char.IsControl(e.KeyChar) _
+                        AndAlso (Not Char.IsDigit(e.KeyChar) _
+                        AndAlso (e.KeyChar <> Microsoft.VisualBasic.ChrW(46)))) Then
+            e.Handled = True
+        End If
+
+        ' only allow one decimal point
+        If ((e.KeyChar = Microsoft.VisualBasic.ChrW(46)) _
+                    AndAlso (CType(sender, TextBox).Text.IndexOf(Microsoft.VisualBasic.ChrW(46)) > -1)) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
