b1d0d9d2ce8e16026586c67b4773d2ceb1a03641
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-04-22 23:33:08 +0800

    Project StartUp page

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BAIT1083_Visual_Programming.Login
+            Me.MainForm = Global.BAIT1083_Visual_Programming.Form_StonksLib
         End Sub
     End Class
 End Namespace
