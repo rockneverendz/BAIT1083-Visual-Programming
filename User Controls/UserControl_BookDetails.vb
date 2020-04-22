08a66b9731e1e32eae703d65889a0553c15d3e91
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-04-23 02:40:57 +0800

    Update UserControl_BookDetails.vb
    + add view history by `All or Selected`

--- User Controls/UserControl_BookDetails.vb
+++ User Controls/UserControl_BookDetails.vb
@@ -1,12 +1,9 @@
 ﻿Public Class UserControl_BookDetails
 
+    Public Property BookList As UserControl_BookList
     Dim bookID As Integer
 
     Public Sub Fill(bookID As Integer)
-
-        ' Clear the list
-        ListView_Copies.Items.Clear()
-        ListView_History.Items.Clear()
 
         Me.bookID = bookID
         Using database As New LibDBDataContext()
@@ -22,47 +19,12 @@
             NumericUpDown_Copies.Value = book.Copies
             TextBox_CallNo.Text = book.Call_no
 
-            ' Get the copies (of the book) details and insert into listView
-            Dim copies = From c In database.Copies
-                         Where c.Book_ID = bookID
-                         Order By c.Copy_ID
-
-            For Each copy In copies
-
-                Dim row(4) As String
-                Dim checkout_ID = copy.CheckOut_ID
-
-                If IsNothing(checkout_ID) Then
-                    row = {copy.Copy_ID, copy.Status, "", ""}
-                Else
-                    Dim duedate = database.CheckOuts.First(Function(o) o.Chk_ID = checkout_ID).Due_Date
-                    row = {copy.Copy_ID, copy.Status, copy.CheckOut_ID, duedate}
-                End If
-
-                ' Insert integers on tag for sorting later
-                Dim lvi = New ListViewItem(row)
-                lvi.SubItems(0).Tag = copy.Copy_ID
-
-                ListView_Copies.Items.Add(lvi)
-            Next
-
-            ' Get the history (of the book) and insert into listView
-            Dim history = From co In database.CheckOuts
-                          Join col In database.CheckOutLists On co.Chk_ID Equals col.Chk_ID
-                          Join c In database.Copies On col.Copy_ID Equals c.Copy_ID
-                          Where c.Book_ID = bookID
-                          Order By co.Due_Date Descending
-                          Select New HistoryModel(c.Copy_ID, co.Chk_ID, co.Issue_Date, co.Due_Date)
-
-            For Each record In history
-                Dim listViewItem = New ListViewItem(record.ToStringArray)
-                ListView_History.Items.Add(listViewItem)
-            Next
+            GetCopies(database)
+            GetHistory(database)
+            RadioButton_All.Checked = True
 
         End Using
     End Sub
-
-    Public Property BookList As UserControl_BookList
 
     Private Sub Button_Cancel_Click(sender As Object, e As EventArgs) Handles Button_Cancel.Click
         Me.Hide()
@@ -101,6 +63,103 @@
 
             database.SubmitChanges()
         End Using
+    End Sub
+
+    Private Sub ListView_Copies_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView_Copies.SelectedIndexChanged
+        If RadioButton_Select.Checked Then
+            RadioButton_History_Click(RadioButton_Select, EventArgs.Empty)
+        End If
+    End Sub
+
+    Private Sub RadioButton_History_Click(sender As Object, e As EventArgs) Handles _
+        RadioButton_All.Click, RadioButton_Select.Click
+        Using database As New LibDBDataContext()
+            If (sender.Equals(RadioButton_All)) Then
+                ' Clear the selected copies and get all copies history
+                ListView_Copies.SelectedItems.Clear()
+                GetHistory(database)
+            ElseIf (sender.Equals(RadioButton_Select)) Then
+                If ListView_Copies.SelectedItems.Count = 1 Then
+                    ' Get the selected copy_ID and get the copy history
+                    Dim copy_ID As Integer = ListView_Copies.SelectedItems(0).SubItems(0).Tag
+                    GetHistory(database, copy_ID)
+                Else
+                    ' No item selected, clear the list
+                    ListView_History.Items.Clear()
+                End If
+            End If
+        End Using
+    End Sub
+
+    Private Sub GetCopies(database As LibDBDataContext)
+        ' Clear the copies list
+        ListView_Copies.Items.Clear()
+
+        ' Get the copies (of the book) details and insert into listView
+        Dim copies = From c In database.Copies
+                     Where c.Book_ID = bookID
+                     Order By c.Copy_ID
+
+        For Each copy In copies
+
+            Dim row(4) As String
+            Dim checkout_ID = copy.CheckOut_ID
+
+            If IsNothing(checkout_ID) Then
+                row = {copy.Copy_ID, copy.Status, "", ""}
+            Else
+                Dim duedate = database.CheckOuts.First(Function(o) o.Chk_ID = checkout_ID).Due_Date
+                row = {copy.Copy_ID, copy.Status, copy.CheckOut_ID, duedate}
+            End If
+
+            ' Insert integers on tag for sorting later
+            Dim lvi = New ListViewItem(row)
+            lvi.SubItems(0).Tag = copy.Copy_ID
+
+            ListView_Copies.Items.Add(lvi)
+        Next
+    End Sub
+
+    Private Sub GetHistory(database As LibDBDataContext)
+        ' Clear the history list
+        ListView_History.Items.Clear()
+
+        ' Get the history (of the book) and insert into listView
+        Dim history = From co In database.CheckOuts
+                      Join col In database.CheckOutLists On co.Chk_ID Equals col.Chk_ID
+                      Join c In database.Copies On col.Copy_ID Equals c.Copy_ID
+                      Where c.Book_ID = bookID
+                      Order By co.Due_Date Descending
+                      Select New HistoryModel(c.Copy_ID, co.Chk_ID, co.Issue_Date, co.Due_Date)
+
+        ' Fill the ListView_History
+        FillHistory(history)
+    End Sub
+
+    Private Sub GetHistory(database As LibDBDataContext, copy_ID As Integer)
+        ' Clear the history list
+        ListView_History.Items.Clear()
+
+        ' Get the history (of the book) and insert into listView
+        Dim history = From co In database.CheckOuts
+                      Join col In database.CheckOutLists On co.Chk_ID Equals col.Chk_ID
+                      Join c In database.Copies On col.Copy_ID Equals c.Copy_ID
+                      Where c.Book_ID = bookID And
+                          c.Copy_ID = copy_ID
+                      Order By co.Due_Date Descending
+                      Select New HistoryModel(c.Copy_ID, co.Chk_ID, co.Issue_Date, co.Due_Date)
+
+        ' Fill the ListView_History
+        FillHistory(history)
+    End Sub
+
+    Private Sub FillHistory(history As IQueryable(Of HistoryModel))
+        For Each record In history
+            Dim listViewItem = New ListViewItem(record.ToStringArray)
+            ListView_History.Items.Add(listViewItem)
+            listViewItem.SubItems(0).Tag = record.Copy_ID
+            listViewItem.SubItems(1).Tag = record.Chk_ID
+        Next
     End Sub
 
     Private Class HistoryModel
