c9df0e4448d35560ee80f098ff87ded7e293f4eb
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-04-25 21:50:05 +0800

    Added Splash Screen

---
+++ StonksLibSplash.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Splash
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.Color.Transparent
+		Me.Label1.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+		Me.Label1.Location = New System.Drawing.Point(29, 236)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(394, 76)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "StonksLib LMS" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Library Management System"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+		Me.Label1.UseWaitCursor = True
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.BackColor = System.Drawing.Color.Transparent
+		Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+		Me.Label2.Location = New System.Drawing.Point(31, 322)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(85, 25)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "Ver 1.00"
+		Me.Label2.UseWaitCursor = True
+		'
+		'Splash
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = Global.BAIT1083_Visual_Programming.My.Resources.Resources.LibBackground
+		Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.ClientSize = New System.Drawing.Size(750, 372)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Name = "Splash"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "StonksLibSplash"
+		Me.UseWaitCursor = True
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+End Class
