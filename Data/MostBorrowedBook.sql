a5a094dd8a7a07d34d20b43ef262a58167aaf988
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-04-26 02:06:45 +0800

    Add Report Query Testing

---
+++ Data/MostBorrowedBook.sql
@@ -0,0 +1,8 @@
+﻿Select Copies.Book_ID, Book.Book_title, COUNT(CheckOut.Chk_ID) as MostCheckedOutBook
+From CheckOutList
+Inner Join CheckOut ON CheckOutList.Chk_ID = CheckOut.Chk_ID
+Inner Join Copies ON CheckOutList.Copy_ID = Copies.Copy_ID 
+Inner Join Book On Copies.Book_ID = Book.Book_ID
+Group by Copies.Book_ID, Book.Book_title
+
+
---
+++ Data/TopMostCheckOutStudent.sql
@@ -0,0 +1,5 @@
+﻿Select Patron.Name, COUNT(CheckOut.Chk_ID) as MostCheckedOutPatron
+From CheckOut
+Inner Join Patron ON Patron.Patron_ID = CheckOut.Patron_ID
+Group by Patron.Name
+Order by MostCheckedOutPatron DESC
