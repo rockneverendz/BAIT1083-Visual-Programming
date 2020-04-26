91c7025d9c7c14d4683acdcd169d62e4838b722b
Cy Soo <36953143+Soocyang@users.noreply.github.com>
2020-04-26 19:48:51 +0800

    Create fineReport.sql
    Fine Rpt Query

---
+++ Data/fineReport.sql
@@ -0,0 +1,6 @@
+﻿Select [Patron].Patron_ID, [Patron].[Name], COUNT([Return].Fine_Amount) as [Number of Fines Record], SUM([Return].Fine_Amount) as Total_Fine
+From CheckOutList
+INNER JOIN [Return] ON [Return].Rtn_ID = CheckOutList.Return_ID
+INNER JOIN [CheckOut] ON [CheckOut].Chk_ID = CheckOutList.Chk_ID
+INNER JOIN [Patron] ON [Patron].Patron_ID = [CheckOut].Patron_ID
+Group By [Patron].Patron_ID, [Patron].[Name]
